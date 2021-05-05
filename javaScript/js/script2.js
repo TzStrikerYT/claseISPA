f026058b7ff856c7bea1dae75ee083fd00edfd6c
Michael Cocuy <maicol.asus@gmail.com>
2021-05-05 17:31:05 -0500

    [add] add advance for basic js

---
+++ javaScript/js/script2.js
@@ -0,0 +1,19 @@
+function comprar() {
+  let gansito = false;
+  let chocoBar = false;
+
+  if (gansito === true) {
+    alert("Toma tu gansito");
+  }
+/* hshshhshs
+jdjdjjdjd
+hdhdhhdjd*/
+  //|| = ó / && = y
+  if (chocoBar === true && gansito !== true) {
+    alert("Toma tu barra de chocorramo");
+  }  
+  
+  if(chocoBar === false && gansito === false){
+    alert("No hay Ninguno");
+  }
+}
